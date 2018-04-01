1af69f2dbf124b868d582c963efab51d5ab574e6
Miroslaw K <webdeveloper@ersonic.com>
2018-04-01 21:16:28 +0200

    Class AppConnection

---
+++ src/AppConnection.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @name AppConnection
+ * @category Epa
+ * @package Library
+ * @subpackage AppConnection
+ * @version 4.3.0
+ * @copyright (c) 2018 E R S O N I C (http://www.ersonic.com)
+ * @license http://www.epa24.pl/license
+ * @link http://www.epa24.pl/wiki
+ */
+namespace Epa;
+
+class AppConnection
+{
+
+}
