54d8e94fa446d89a037fa22c0e35c1bac480e2f0
Miroslaw K <webdeveloper@ersonic.com>
2018-04-01 21:39:31 +0200

    Class AppVisit

---
+++ src/AppVisit.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @name AppVisit
+ * @category Epa
+ * @package Library
+ * @subpackage AppVisit
+ * @version 4.3.0
+ * @author Miroslaw Kukuryka
+ * @copyright (c) 2018 E R S O N I C (http://www.ersonic.com)
+ * @license http://www.epa24.pl/license
+ * @link http://www.epa24.pl/wiki
+ */
+namespace Epa;
+
+class AppVisit extends AppConnection
+{
+}
