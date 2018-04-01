098acc8930b777c07004da0d0ea7acc47c836969
Miroslaw K <webdeveloper@ersonic.com>
2018-04-01 21:58:21 +0200

    Class AppSeo

---
+++ src/AppSeo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @name AppSeo
+ * @category Epa
+ * @package Library
+ * @subpackage AppSeo
+ * @version 4.3.0
+ * @copyright (c) 2018 E R S O N I C (http://www.ersonic.com)
+ * @license http://www.epa24.pl/license
+ * @link http://www.epa24.pl/wiki
+ */
+namespace Epa;
+
+class AppSeo
+{
+
+}
