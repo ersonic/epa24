8737b77cbeb9d8a058caec9110cfb3ae702fbd6b
Miroslaw K <webdeveloper@ersonic.com>
2018-04-02 11:02:45 +0200

    Class Exception

---
+++ src/Exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @name Exception
+ * @category Epa
+ * @package Library
+ * @subpackage Exception
+ * @version 4.3.0
+ * @author Miroslaw Kukuryka
+ * @copyright (c) 2018 E R S O N I C (http://www.ersonic.com)
+ * @license http://www.epa24.pl/license
+ * @link http://www.epa24.pl/wiki
+ */
+namespace Epa;
+
+class Exception extends \Exception
+{
+}
