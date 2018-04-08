0be622903a26626bd84bafa6a636f2c3747ed295
Miroslaw K <webdeveloper@ersonic.com>
2018-04-08 13:36:39 +0200

    Class AppSeo

--- src/AppSeo.php
+++ src/AppSeo.php
@@ -13,4 +13,96 @@ namespace Epa;
 
 class AppSeo
 {
+
+    /**
+     *
+     * @var string
+     */
+    private $_seoTitle = 'Application Skeleton';
+
+    /**
+     *
+     * @var string
+     */
+    private $_seoKeywords = '';
+
+    /**
+     *
+     * @var string
+     */
+    private $_seoDescription = 'Application Skeleton';
+
+    /**
+     */
+    public function prepare()
+    {
+        $params = [];
+        return sha1(implode('/', $params));
+    }
+
+    /**
+     *
+     * @name setSeoTitle
+     * @access public
+     * @param string $value            
+     */
+    public function setSeoTitle($value)
+    {
+        $this->_seoTitle = $value;
+    }
+
+    /**
+     *
+     * @name getSeoTitle
+     * @access public
+     * @return string
+     */
+    public function getSeoTitle()
+    {
+        return $this->_seoTitle;
+    }
+
+    /**
+     *
+     * @name setSeoKeywords
+     * @access public
+     * @param string $value            
+     */
+    public function setSeoKeywords($value)
+    {
+        $this->_seoKeywords = $value;
+    }
+
+    /**
+     *
+     * @name getSeoKeywords
+     * @access public
+     * @return string
+     */
+    public function getSeoKeywords()
+    {
+        $this->_seoKeywords;
+    }
+
+    /**
+     *
+     * @name setSeoDescription
+     * @access public
+     * @return string
+     */
+    public function setSeoDescription($value)
+    {
+        $this->_seoDescription = $value;
+    }
+
+    /**
+     *
+     * @name getSeoDescription
+     * @access public
+     * @return string
+     */
+    public function getSeoDescription()
+    {
+        return $this->_seoDescription;
+    }
 }
