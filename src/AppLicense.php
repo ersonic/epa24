6a70b07d9dd917541e325586e8a9db5c9662a47e
Miroslaw K <webdeveloper@ersonic.com>
2018-04-08 13:42:46 +0200

    Class AppLicense

---
+++ src/AppLicense.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @name AppLicense
+ * @category Epa
+ * @package Library
+ * @subpackage AppLicense
+ * @version 4.3.0
+ * @copyright (c) 2018 E R S O N I C (http://www.ersonic.com)
+ * @license http://www.epa24.pl/license
+ * @link http://www.epa24.pl/wiki
+ */
+namespace Epa;
+
+class AppLicense
+{
+    
+}
