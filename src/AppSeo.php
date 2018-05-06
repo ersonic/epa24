502ae4f1139f72035cad65c5042cf57ac20bad41
Miroslaw K <webdeveloper@ersonic.com>
2018-05-06 12:04:55 +0200

    AppSeo - add new merhod

--- src/AppSeo.php
+++ src/AppSeo.php
@@ -16,39 +16,153 @@ class AppSeo
 
     /**
      *
-     * @var string
+     * @var array
      */
-    private $_seoTitle = 'Application Skeleton';
+    private static $_seoParams = [];
 
     /**
      *
      * @var string
      */
-    private $_seoKeywords = '';
+    private static $_seoLocale = 'pl_PL';
 
     /**
      *
      * @var string
      */
-    private $_seoDescription = 'Application Skeleton';
+    private static $_seoDirPath;
 
     /**
+     *
+     * @var string
      */
-    public function prepare()
+    private static $_seoHash;
+
+    /**
+     *
+     * @var string
+     */
+    private static $_seoTitle = 'Application Skeleton';
+
+    /**
+     *
+     * @var string
+     */
+    private static $_seoKeywords = 'CMS, Application, PHP, CRM, Pages';
+
+    /**
+     *
+     * @var string
+     */
+    private static $_seoDescription = 'Application Skeleton';
+
+    /**
+     *
+     * @name setSeoLocale
+     * @access public
+     * @param string $value
+     * @return string
+     */
+    public static function setSeoLocale($value)
     {
-        $params = [];
-        return sha1(implode('/', $params));
+        return self::$_seoLocale;
+    }
+
+    /**
+     *
+     * @name getSeoLocale
+     * @access public
+     * @return string
+     */
+    public static function getSeoLocale()
+    {
+        return self::$_seoLocale;
+    }
+
+    /**
+     *
+     * @param string $key
+     * @param string $value
+     * @return array
+     */
+    public static function addSeoParam($key, $value)
+    {
+        return self::$_seoParams[$key] = $value;
+    }
+
+    /**
+     *
+     * @name getSeoParam
+     * @access public
+     * @param string $key
+     * @return string
+     */
+    public static function getSeoParam($key)
+    {
+        if (self::hasSeoParam($key)) {
+            return self::$_seoParams[$key];
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     *
+     * @param unknown $value
+     * @return unknown
+     */
+    public function setSeoDirPath($value)
+    {
+        return self::$_seoDirPath = $value;
+    }
+
+    /**
+     *
+     * @name getSeoDirPath
+     * @access public
+     * @return string
+     */
+    public function getSeoDirPath()
+    {
+        return self::$_seoDirPath;
+    }
+
+    /**
+     *
+     * @name hasSeoParam
+     * @access public
+     * @param string $key
+     * @return boolean
+     */
+    public static function hasSeoParam($key)
+    {
+        if (isset(self::$_seoParams[$key])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @name getSeoParams
+     * @access public
+     * @return array
+     */
+    public static function getSeoParams()
+    {
+        return self::$_seoParams;
     }
 
     /**
      *
      * @name setSeoTitle
      * @access public
-     * @param string $value            
+     * @param string $value
      */
-    public function setSeoTitle($value)
+    public static function setSeoTitle($value)
     {
-        $this->_seoTitle = $value;
+        return self::$_seoTitle = $value;
     }
 
     /**
@@ -57,20 +171,20 @@ class AppSeo
      * @access public
      * @return string
      */
-    public function getSeoTitle()
+    public static function getSeoTitle()
     {
-        return $this->_seoTitle;
+        return self::$_seoTitle;
     }
 
     /**
      *
      * @name setSeoKeywords
      * @access public
-     * @param string $value            
+     * @param string $value
      */
-    public function setSeoKeywords($value)
+    public static function setSeoKeywords($value)
     {
-        $this->_seoKeywords = $value;
+        return self::$_seoKeywords = $value;
     }
 
     /**
@@ -79,9 +193,9 @@ class AppSeo
      * @access public
      * @return string
      */
-    public function getSeoKeywords()
+    public static function getSeoKeywords()
     {
-        $this->_seoKeywords;
+        return self::$_seoKeywords;
     }
 
     /**
@@ -90,9 +204,9 @@ class AppSeo
      * @access public
      * @return string
      */
-    public function setSeoDescription($value)
+    public static function setSeoDescription($value)
     {
-        $this->_seoDescription = $value;
+        return self::$_seoDescription = $value;
     }
 
     /**
@@ -101,8 +215,79 @@ class AppSeo
      * @access public
      * @return string
      */
-    public function getSeoDescription()
+    public static function getSeoDescription()
     {
-        return $this->_seoDescription;
+        return self::$_seoDescription;
+    }
+
+    /**
+     *
+     * @name setSeoHash
+     * @access public
+     * @param string $value
+     * @return string
+     */
+    private static function setSeoHash($value)
+    {
+        return self::$_seoHash = $value;
+    }
+
+    /**
+     *
+     * @name getSeoHash
+     * @access public
+     * @return string
+     */
+    public static function getSeoHash()
+    {
+        return self::$_seoHash;
+    }
+
+    /**
+     *
+     * @name init
+     * @access public
+     */
+    public static function init()
+    {
+        $params = [];
+        $tmp = [
+            'module',
+            'controller',
+            'action'
+        ];
+        foreach ($tmp as $key) {
+            if (self::hasSeoParam($key) !== false) {
+                $params[] = self::$_seoParams[$key];
+                unset(self::$_seoParams[$key]);
+            }
+        }
+        if (sizeof(self::getSeoParams())) {
+            $tmp = self::getSeoParams();
+            foreach ($tmp as $key => $value) {
+                if (self::hasSeoParam($key) !== false) {
+                    $params[] = $key . '/' . self::$_seoParams[$key];
+                    unset(self::$_seoParams[$key]);
+                }
+            }
+        }
+        
+        self::setSeoHash(sha1(implode('/', $params)));
+        if (file_exists(self::getSeoDirPath() . self::getSeoLocale() . '.php')) {
+            require_once self::getSeoDirPath() . self::getSeoLocale() . '.php';
+            $arr = 'seo_' . self::getSeoLocale();
+            
+            if (isset(${$arr}[self::getSeoHash()][1])) {
+                self::setSeoTitle($seo_pl_PL[self::getSeoHash()][1]);
+            }
+            
+            if (isset(${$arr}[self::getSeoHash()][2])) {
+                self::setSeoKeywords($seo_pl_PL[self::getSeoHash()][2]);
+            }
+            
+            if (isset(${$arr}[self::getSeoHash()][3])) {
+                self::setSeoDescription($seo_pl_PL[self::getSeoHash()][3]);
+            }
+        }
     }
 }
